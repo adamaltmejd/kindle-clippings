3cb96aacf5aaacf155ca985cc76ea1d56adcafe8
Adam Altmejd <adam@altmejd.se>
2015-11-17 14:46:52 +0100

    Update to export Markdown files.

---
+++ README.md
@@ -0,0 +1,21 @@
+Kindle Clippings
+================
+
+A simple python script to extract clippings from `My Clippings.txt, organize, store and output them in a more elegant way.
+
+
+Features
+--------
+
+Automator workflow automatically imports the `My Clippings.txt` file and the script creates a markdown note for each item.
+
+
+Usage
+-----
+
+Install the automator workflow as a folder action watching the /Volumes/ folder. Make sure to chose where it should copy your clips, and that the script is in the same folder.
+
+
+To do
+-----
+Own notes are currently treated as highlights. Should rather be formatted to accompany the relevant highlight.
--- kindle.py
+++ kindle.py
@@ -1,60 +1,91 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python
 # -*- coding: utf-8 -*-
+
+"""
+Kindle Clippings converter.
+
+Fork of (https://github.com/lxyu/kindle-clippings)
+
+This script will fetch My Clippings data and output it into different txt files
+for each book.
+"""
 
 import collections
 import json
 import os
-import re
+from datetime import datetime
 
 BOUNDARY = u"==========\r\n"
 DATA_FILE = u"clips.json"
+INPUT_FILE = u"My Clippings.txt"
 OUTPUT_DIR = u"output"
 
 
 def get_sections(filename):
+    """Read the sections, split by BOUNDARY."""
     with open(filename, 'r') as f:
         content = f.read().decode('utf-8')
-    content = content.replace(u'\ufeff', u'')
-    return content.split(BOUNDARY)
+
+    content = content.replace(u'\ufeff', u'') # remove BOM char
+    return filter(None, content.split(BOUNDARY)) # filter out empty lines
 
 
 def get_clip(section):
+    """Fetch each clip."""
     clip = {}
 
-    lines = [l for l in section.split(u'\r\n') if l]
-    if len(lines) != 3:
-        return
+    lines = [l.strip() for l in section.splitlines() if l]
 
-    clip['book'] = lines[0]
-    match = re.search(r'(\d+)-\d+', lines[1])
-    if not match:
-        return
-    position = match.group(1)
+    try:
+        pos = lines[1].split(' | Added on ')[0]
+        if 'on page' in pos.lower():
+            pages = pos.split(' | ')[0][pos.lower().find('on page') + 8:]
+            location = pos.split(' | ')[1][9:]
+            id = int(location.split('-')[0])
+            pos = u'Page: %s (Location: %s)' % (pages, location)
+        elif 'location' in pos.lower():
+            location = pos[pos.lower().find('location') + 9:]
+            id = int(location.split('-')[0])
+            pos = u'Location: %s' % location
 
-    clip['position'] = int(position)
-    clip['content'] = lines[2]
+        clip['title'] = lines[0]
+        clip['id'] = id
+        clip['pos'] = pos
 
-    return clip
+        try:
+            clip['date'] = datetime.strptime(
+                lines[1].split(' | Added on ')[1],
+                '%A, %B %d, %Y %I:%M:%S %p'
+            ).isoformat()
+        except:
+            clip['date'] = '<error>'
+
+        clip['content'] = u'' if(len(lines) < 3) else lines[2]
+
+        return clip
+    except:
+        print(u'Error fetching clip in section:\n\n' + section.encode('utf-8'))
 
 
 def export_txt(clips):
-    """
-    Export each book's clips to single text.
-    """
+    """Export each book's clips to single text."""
     for book in clips:
-        lines = []
-        for pos in sorted(clips[book]):
-            lines.append(clips[book][pos].encode('utf-8'))
+        lines = ['# ' + book.encode('utf-8')]
+        for id in sorted(clips[book]):
+            heading = '### ' + clips[book][id][0] + '\n'
+            date = 'Saved on: ' + clips[book][id][1][0:10] + '\n'
+            content = ('\n>\t' + clips[book][id][2]
+                       if(len(clips[book][id][2]) > 0) else '_Bookmark_')
+            text = heading + date + content
+            lines.append(text.encode('utf-8'))
 
-        filename = os.path.join(OUTPUT_DIR, u"%s.txt" % book)
+        filename = os.path.join(OUTPUT_DIR, '%s.md' % book)
         with open(filename, 'w') as f:
-            f.write("\n\n--\n\n".join(lines))
+            f.write('\n\n'.join(lines))
 
 
 def load_clips():
-    """
-    Load previous clips from DATA_FILE
-    """
+    """Load previous clips from DATA_FILE."""
     try:
         with open(DATA_FILE, 'r') as f:
             return json.load(f)
@@ -63,27 +94,25 @@ def load_clips():
 
 
 def save_clips(clips):
-    """
-    Save new clips to DATA_FILE
-    """
+    """Save new clips to DATA_FILE."""
     with open(DATA_FILE, 'wb') as f:
         json.dump(clips, f)
 
 
 def main():
-    # load old clips
+    """Main task."""
+
+    # Load old clips
     clips = collections.defaultdict(dict)
     clips.update(load_clips())
 
-    # extract clips
-    sections = get_sections(u'My Clippings.txt')
+    # Extract clips
+    sections = get_sections(INPUT_FILE)
     for section in sections:
         clip = get_clip(section)
         if clip:
-            clips[clip['book']][clip['position']] = clip['content']
-
-    # remove key with empty value
-    clips = {k: v for k, v in clips.iteritems() if v}
+            clips[clip['title']][clip['id']] = [clip['pos'], clip['date'],
+                                                clip['content']]
 
     # save/export clips
     save_clips(clips)
